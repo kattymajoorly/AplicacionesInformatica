1d56366a107cf7dfb85b3159c67937d0d7d5594d
Josefa241092 <josefa241092@gmail.com>
2015-07-08 21:17:12 -0500

    Creacion del metodo para llamar la consulta desde el servicio XD

--- ServicioWebPrueba/src/com/upse/servicio/ServicioWeb.java
+++ ServicioWebPrueba/src/com/upse/servicio/ServicioWeb.java
@@ -1,6 +1,12 @@
 package com.upse.servicio;
 
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import com.upse.entidades.DatosLogin;
+import com.upse.entidades.Productos;
+import com.upse.modelo.DBProducto;
 import com.upse.modelo.DBUsuario;
 
 public class ServicioWeb {
@@ -35,6 +41,37 @@ public class ServicioWeb {
 		res = s.ingresarLogin(usuario, password);
 		
 		return res;
+	}
+	
+	public String consultaProducto(String nombreProducto){
+		String resultado = "[";
+		Integer contador = 0;
+		
+		DBProducto dbproductos = new DBProducto();
+		ArrayList<Productos> res = dbproductos.consultaProducto(nombreProducto);
+		
+		//lisIteraor devuelve un arreglo de todos los indices de la lista
+		//lisIterator recorre todo la lista
+		
+		//variable de tipo Iterator es para recorrer la lista
+		// se pueden usar otros metodos como for o foreach pero el mas optimo el Iterator.
+		Iterator r = res.listIterator();
+		while(r.hasNext()){
+			contador = contador + 1;
+			Productos pro = (Productos) r.next();
+			//contador ==1 para la concatenacion  xq el primer objeto no lleva coma entonces si eso es asi
+			//noce pone la coma
+			if (contador== 1){
+				resultado = resultado + pro.toJsonCDProducto();
+			}
+			
+			//si se pone contador !=1 entonces en el array se le pone la coma
+			if (contador != 1){
+				resultado = resultado + ", " +pro.toJsonCDProducto();
+			}
+			
+		}
+		return resultado + "]";
 	}
 	
 	
