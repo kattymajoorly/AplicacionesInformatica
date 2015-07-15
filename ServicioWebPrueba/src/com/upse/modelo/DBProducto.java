f1381bbbb412e24fd19776dca4bf9800200867f6
kattymajoorly <elizakatty25@gmail.com>
2015-07-15 15:56:35 -0500

    Correcion de los metodos para la consulta de Productos

--- ServicioWebPrueba/src/com/upse/modelo/DBProducto.java
+++ ServicioWebPrueba/src/com/upse/modelo/DBProducto.java
@@ -15,6 +15,7 @@ import com.upse.entidades.Productos;
 
 public class DBProducto {
 	
+	//se debe buscar por categoria, nombre de producto, idcategoria ==> (idproducto,descripcion, precio), general ==> muestra categoria 
 	
 	
 	public ArrayList<Productos> consultaProducto(String nombreProducto){
@@ -37,10 +38,7 @@ public class DBProducto {
 			sql = "select pro.id_productos, pro.nombre_producto, pro.descripcion, cat.id_categoria ,cat.nombre_categoria, pro.precio, pro.stock_minimo, pro.stock_actual " +
 					"from productos pro inner join categoria cat on pro.id_categoria = cat.id_categoria and pro.nombre_producto like '%"+nombreProducto+"%'";
 			
-			
-			System.out.println(sql);
-			
-			
+					
 			//ejecutar el query
 			try {
 				sentencia = con.createStatement();
@@ -80,5 +78,181 @@ public class DBProducto {
 		return lista;
         
 	}
+	
+public ArrayList<Categoria> consultaProductoCategoria(){
+		
+		ArrayList<Categoria> lista = null;
+		
+		//1. Conectarse a la bdd
+			DBManager dbm = new DBManager();
+			Connection con = dbm.getConection();
+			if(con == null){
+				System.out.println("Conexion es null");
+			}
+				
+			//sentencia a ejecutar
+	        Statement sentencia;
+	        //objeto para almacenar resultados
+	        ResultSet resultados = null;
+			String sql = null;	
+			
+			sql = "select * from categoria";
+			
+					
+			//ejecutar el query
+			try {
+				sentencia = con.createStatement();
+				resultados = sentencia.executeQuery(sql);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Error en ejecución de sentencia");
+				e.printStackTrace();
+			}
+			
+		try {
+			//recorrer los resultados que haya arrojado los select
+			lista =new ArrayList<Categoria>();
+			
+			while(resultados.next()){
+				Categoria listaCategoria = new Categoria();
+				
+				listaCategoria.setIdCategoria(resultados.getInt("id_categoria"));
+				listaCategoria.setNombre_categoria(resultados.getString("nombre_categoria"));
+				
+							
+				lista.add(listaCategoria);
+			}
+		}catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		
+		return lista;
+        
+	}
+
+public ArrayList<Productos> consultaProductoxCategoria(Integer idCategoria){
+	
+	ArrayList<Productos> lista = null;
+	
+	//1. Conectarse a la bdd
+		DBManager dbm = new DBManager();
+		Connection con = dbm.getConection();
+		if(con == null){
+			System.out.println("Conexion es null");
+		}
+			
+		//sentencia a ejecutar
+        Statement sentencia;
+        //objeto para almacenar resultados
+        ResultSet resultados = null;
+		String sql = null;	
+		
+		sql = "SELECT pro.id_productos, pro.nombre_producto, cat.id_categoria, cat.nombre_categoria FROM productos pro, categoria cat " +
+				"WHERE cat.id_categoria=pro.id_categoria and cat.id_categoria="+idCategoria+"";
+		
+		//ejecutar el query
+		try {
+			sentencia = con.createStatement();
+			resultados = sentencia.executeQuery(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error en ejecución de sentencia");
+			e.printStackTrace();
+		}
+		
+	try {
+		//recorrer los resultados que haya arrojado los select
+		lista =new ArrayList<Productos>();
+		
+		while(resultados.next()){
+			Categoria listaCategoria = new Categoria();
+			Productos listaProducto = new Productos();
+			
+			listaCategoria.setIdCategoria(resultados.getInt("id_categoria"));
+			listaCategoria.setNombre_categoria(resultados.getString("nombre_categoria"));
+			listaProducto.setCategoria(listaCategoria);
+			listaProducto.setIdProductos(resultados.getInt("pro.id_productos"));
+			listaProducto.setNombre_producto(resultados.getString("pro.nombre_producto"));
+			
+		
+			lista.add(listaProducto);
+		}
+	}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	
+	return lista;
+    
+}
+
+
+public ArrayList<Productos> consultaProductoDetalle(Integer idProducto){
+	
+	ArrayList<Productos> lista = null;
+	
+	//1. Conectarse a la bdd
+		DBManager dbm = new DBManager();
+		Connection con = dbm.getConection();
+		if(con == null){
+			System.out.println("Conexion es null");
+		}
+			
+		//sentencia a ejecutar
+        Statement sentencia;
+        //objeto para almacenar resultados
+        ResultSet resultados = null;
+		String sql = null;	
+		
+		sql = "SELECT pro.id_productos, pro.nombre_producto, pro.descripcion, pro.precio, cat.id_categoria " +
+				"FROM productos pro, categoria cat WHERE cat.id_categoria=pro.id_categoria and pro.id_productos="+idProducto+"";
+		System.out.println(sql);
+		
+				
+		//ejecutar el query
+		try {
+			sentencia = con.createStatement();
+			resultados = sentencia.executeQuery(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error en ejecución de sentencia");
+			e.printStackTrace();
+		}
+		
+	try {
+		//recorrer los resultados que haya arrojado los select
+		lista =new ArrayList<Productos>();
+		
+		while(resultados.next()){
+			Productos listaProducto = new Productos();
+			Categoria listaCategoria = new Categoria();
+			listaCategoria.setIdCategoria(resultados.getInt("cat.id_categoria"));
+			listaProducto.setCategoria(listaCategoria);
+			
+			listaProducto.setIdProductos(resultados.getInt("pro.id_productos"));
+			listaProducto.setNombre_producto(resultados.getString("pro.nombre_producto"));
+			listaProducto.setDescripcion(resultados.getString("pro.descripcion"));
+			listaProducto.setPrecio(resultados.getDouble("pro.precio"));
+			
+		
+			lista.add(listaProducto);
+		}
+	}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	
+	return lista;
+    
+}
+
+
+
+	
+	
 	
 }
