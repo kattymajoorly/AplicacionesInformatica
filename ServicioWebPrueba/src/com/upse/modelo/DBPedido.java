7754c850c903506d35b36d6045c4995be7069109
kattymajoorly <elizakatty25@gmail.com>
2015-07-30 15:07:18 -0500

    Modeificado DBPedido

--- ServicioWebPrueba/src/com/upse/modelo/DBPedido.java
+++ ServicioWebPrueba/src/com/upse/modelo/DBPedido.java
@@ -13,160 +13,220 @@ import com.upse.entidades.DetallePedido;
 import com.upse.entidades.Productos;
 
 public class DBPedido {
-	
-	public Integer nuevoPedidoCabecera(Integer id_usuario, Date fecha, Double subtotal, Double iva, Double total){
-		Integer resultado=0;
-		//p_id_usuario integer, p_fecha date, p_subt double, p_iva double, p_total double
-		DBManager dbm = new DBManager();
-		Connection con = dbm.getConection();
 		
-		//Manejo de transaccion
-		try {
-			con.setAutoCommit(false);
-			String sql = null;
-			sql="CALL sp_pedidos (?,?,?,?,?)";
-			PreparedStatement pstm = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
-			
-			pstm.setInt(1, id_usuario);
-			pstm.setDate(2, (java.sql.Date) fecha);
-			pstm.setDouble(3, subtotal);
-			pstm.setDouble(4, iva);
-			pstm.setDouble(5, total);
+		public Integer nuevoPedidoCabecera(Integer id_usuario, String fecha, Double subtotal, Double iva, Double total,Connection con){
+			Integer resultado=0;
+			//p_id_usuario integer, p_fecha date, p_subt double, p_iva double, p_total double
+			//Manejo de transaccion
+			try {
+				String sql = null;
+				sql="CALL sp_pedidos (?,?,?,?,?)";
+				PreparedStatement pstm = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+				
+				pstm.setInt(1, id_usuario);
+				pstm.setString(2, fecha);
+				pstm.setDouble(3, subtotal);
+				pstm.setDouble(4, iva);
+				pstm.setDouble(5, total);
+				
+				//retorna el numero de filas afectadas
+				int num = pstm.executeUpdate();
+				//si hasta aqui todo ha ido bien commit a la transaccion
+				if(num>0){
+				resultado = 1;
+				
+				}
+				
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				resultado=0;
+				try {
+					con.rollback();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			return resultado;
+
+		}
 		
-		
-			
-			//retorna el numero de filas afectadas
-			int num = pstm.executeUpdate();
-			//si hasta aqui todo ha ido bien commit a la transaccion
-			if(num>0){
-			con.commit();
-			resultado = 1;
-			
+		public Integer stockminimo(Integer id_producto){
+			DBManager dbm = new DBManager();
+			Connection con = dbm.getConection();
+			if(con == null){
+				System.out.println("Conexion es null");
+			}
+				
+			//sentencia a ejecutar
+	        Statement sentencia;
+	        //objeto para almacenar resultados
+	        ResultSet resultados = null;
+			String sql = null;	
+			int lista=0;
+			sql = "select p.stock_minimo from productos p where p.id_productos=" +id_producto;		
+			//ejecutar el query
+			try {
+				sentencia = con.createStatement();
+				resultados = sentencia.executeQuery(sql);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Error en ejecución de sentencia");
+				e.printStackTrace();
 			}
 			
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			resultado=0;
+		try {
+			//recorrer los resultados que haya arrojado los select
+			while(resultados.next()){
+				lista=resultados.getInt("stock_minimo");	
+			}
+		}catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return lista;
+		}
+		
+		public Integer stockactual(Integer id_producto){
+			DBManager dbm = new DBManager();
+			Connection con = dbm.getConection();
+			if(con == null){
+				System.out.println("Conexion es null");
+			}
+			//sentencia a ejecutar
+	        Statement sentencia;
+	        //objeto para almacenar resultados
+	        ResultSet resultados = null;
+			String sql = null;	
+			int lista=0;
+			sql = "select p.stock_actual from productos p where p.id_productos=" +id_producto;		
+			//ejecutar el query
 			try {
-				con.rollback();
+				sentencia = con.createStatement();
+				resultados = sentencia.executeQuery(sql);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Error en ejecución de sentencia");
+				e.printStackTrace();
+			}
+			
+		try {
+			//recorrer los resultados que haya arrojado los select
+			while(resultados.next()){
+				lista=resultados.getInt("stock_actual");	
+			}
+		}catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return lista;
+		}
+		
+		public Integer nuevoPedidoDetalle(ArrayList<DetallePedido> lista, Integer id_pedidos, Connection con){
+			Integer resultado=0;
+			//p_id_usuario integer, p_fecha date, p_subt double, p_iva double, p_total double
+			
+			int num=0;
+			//Manejo de transaccion
+			try {
+					
+				String sql = null;
+				for(int i=0; i<lista.size(); i++)
+				{
+				sql="CALL sp_detalle_pedido (?,?,?,?)";
+				PreparedStatement pstm = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+				
+				pstm.setInt(1, lista.get(i).getIdProductos());
+				pstm.setInt(2, id_pedidos);
+				pstm.setInt(3, lista.get(i).getCantidad());
+				pstm.setDouble(4, lista.get(i).getSubtotal());
+			
+				//retorna el numero de filas afectadas
+				num = pstm.executeUpdate();
+				//si hasta aqui todo ha ido bien commit a la transaccion
+				}
+				if(num>0){
+				resultado = 1;
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				resultado=0;
+				try {
+					con.rollback();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			
+				
+			
+			return resultado;
+
+		}
+		
+		public Integer pedidoGuardado(Integer id_usuario, String fecha, Double subtotal, Double iva, Double total, ArrayList<DetallePedido> listaDetalles){
+			Integer idCabecera = 0;
+			Integer Resultado = 0, resultadocabecera=0, resultadodetalle=0;
+			DBManager dbm = new DBManager();
+			Connection con = dbm.getConection();
+			if(con == null){
+				System.out.println("Conexion es null");
+			}
+			//sentencia a ejecutar
+	        Statement sentencia;
+	        //objeto para almacenar resultados
+	        ResultSet resultados = null;
+			String sql = null;	
+			
+			try {
+				con.setAutoCommit(false);
 			} catch (SQLException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-		}
-		
 			
-		
-		return resultado;
-
-	}
-	
-	public Integer nuevoPedidoDetalle(ArrayList<DetallePedido> lista, Integer id_pedidos){
-		Integer resultado=0;
-		//p_id_usuario integer, p_fecha date, p_subt double, p_iva double, p_total double
-		DBManager dbm = new DBManager();
-		Connection con = dbm.getConection();
-		int num=0;
-		//Manejo de transaccion
-		try {
-			con.setAutoCommit(false);
+			sql = "select max(id_pedidos) as idcabecera from pedidos where estado='A'";
 			
-			String sql = null;
-			
-			for(int i=0; i<lista.size(); i++)
-			{
-				
-			
-			sql="CALL sp_detalle_pedido (?,?,?,?)";
-			PreparedStatement pstm = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
-			
-			pstm.setInt(1, lista.get(i).getIdProductos());
-			pstm.setInt(2, id_pedidos);
-			pstm.setInt(3, lista.get(i).getCantidad());
-			pstm.setDouble(4, lista.get(i).getSubtotal());
-		
-			//retorna el numero de filas afectadas
-			num = pstm.executeUpdate();
-			//si hasta aqui todo ha ido bien commit a la transaccion
-			}
-			if(num>0){
-			con.commit();
-			resultado = 1;
-			
-			}
-			
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			resultado=0;
-			try {
-				con.rollback();
-			} catch (SQLException e1) {
+			resultadocabecera=nuevoPedidoCabecera(id_usuario, fecha, subtotal, iva, total,con);		
+			if (resultadocabecera==1){
+				//ejecutar el query
+				try {
+					sentencia = con.createStatement();
+					resultados = sentencia.executeQuery(sql);
+				} catch (SQLException e) {
 				// TODO Auto-generated catch block
-				e1.printStackTrace();
+					System.out.println("Error en ejecución de sentencia");
+					e.printStackTrace();
+				}
+				try {
+					while(resultados.next()){
+					idCabecera=resultados.getInt("idcabecera");
+				}
+				}catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				}
+				resultadodetalle=nuevoPedidoDetalle(listaDetalles,idCabecera,con);
+				if(resultadodetalle==1){
+					Resultado=1;
+				}else{
+					Resultado=0;
+				}
+			}else{
+				try {
+					con.rollback();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
-		}
-		
 			
-		
-		return resultado;
-
-	}
-	
-	public Integer pedidoGuardado(Integer id_usuario, Date fecha, Double subtotal, Double iva, Double total, ArrayList<DetallePedido> listaDetalles){
-		Integer idCabecera = 0;
-		Integer Resultado = 0, resultadocabecera=0, resultadodetalle=0;
-		DBManager dbm = new DBManager();
-		Connection con = dbm.getConection();
-		if(con == null){
-			System.out.println("Conexion es null");
-		}
-			
-		//sentencia a ejecutar
-        Statement sentencia;
-        //objeto para almacenar resultados
-        ResultSet resultados = null;
-		String sql = null;	
-		
-		sql = "select max(id_pedidos) as idcabecera from pedidos where estado='A'";
-		
-		resultadocabecera=nuevoPedidoCabecera(id_usuario, fecha, subtotal, iva, total);
+			return Resultado;
 				
-		//ejecutar el query
-		try {
-			sentencia = con.createStatement();
-			resultados = sentencia.executeQuery(sql);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Error en ejecución de sentencia");
-			e.printStackTrace();
 		}
-		
-	try {
-		//recorrer los resultados que haya arrojado los select
-		//lista =new ArrayList<Productos>();
-		
-		while(resultados.next()){
-			idCabecera=resultados.getInt("idcabecera");
-		}
-	}catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		resultadodetalle=nuevoPedidoDetalle(listaDetalles,idCabecera);
-		
-		if(resultadocabecera==1 && resultadodetalle==1)
-		{
-			Resultado=1;
-			
-		}else{
-			Resultado=0;
-		}
-		return Resultado;
-			
-	}
 
 }
