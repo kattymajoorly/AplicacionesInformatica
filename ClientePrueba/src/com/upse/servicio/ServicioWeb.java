9f11f7aaf63d9608dd48f1011c608bde2f00c078
Josefa241092 <josefa241092@gmail.com>
2015-07-31 15:31:38 -0500

    Servicio actualizado para consulta pedido

--- ClientePrueba/src/com/upse/servicio/ServicioWeb.java
+++ ClientePrueba/src/com/upse/servicio/ServicioWeb.java
@@ -8,11 +8,12 @@
 package com.upse.servicio;
 
 public interface ServicioWeb extends java.rmi.Remote {
+    public java.lang.String registrousuario(java.lang.String nombres, java.lang.String apellidos, java.lang.String cedula, java.lang.String email, java.lang.String direccion, java.lang.String telefono, int id_tipousuario, java.lang.String alias, java.lang.String dpassword) throws java.rmi.RemoteException;
+    public java.lang.String consultaProducto(java.lang.String nombreProducto) throws java.rmi.RemoteException;
+    public java.lang.String validar_stock(int id_producto, int cantidad) throws java.rmi.RemoteException;
+    public java.lang.String iniciosesion(java.lang.String usuario, java.lang.String password) throws java.rmi.RemoteException;
+    public java.lang.String guardarpedido(int id_usuario, java.lang.String fecha, double subtotal, double iva, double total, java.lang.String jsondetalle) throws java.rmi.RemoteException;
     public java.lang.String consultaProductoCategoria() throws java.rmi.RemoteException;
     public java.lang.String consultaProductoxCategoria(int idCategoria) throws java.rmi.RemoteException;
-    public java.lang.String consultaProducto(java.lang.String nombreProducto) throws java.rmi.RemoteException;
-    public java.lang.String registrousuario(java.lang.String nombres, java.lang.String apellidos, java.lang.String cedula, java.lang.String email, java.lang.String direccion, java.lang.String telefono, int id_tipousuario, java.lang.String alias, java.lang.String dpassword) throws java.rmi.RemoteException;
-    public java.lang.String guardarpedido(int id_usuario, java.lang.String fecha, double subtotal, double iva, double total, java.lang.String jsondetalle) throws java.rmi.RemoteException;
-    public java.lang.String iniciosesion(java.lang.String usuario, java.lang.String password) throws java.rmi.RemoteException;
     public java.lang.String consultaProductoDetalle(int idProducto) throws java.rmi.RemoteException;
 }
